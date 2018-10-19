faac515d153a32cea390e98c6f71e098d1649b15
APrimikirio <a.d.primikirio@gmail.com>
2018-10-19 23:23:44 +0300

    add mapping

--- RTNetCore/RTNetCore/Startup.cs
+++ RTNetCore/RTNetCore/Startup.cs
@@ -21,18 +21,27 @@ namespace RTNetCore
         }
         public void Configure(IApplicationBuilder app)
         {
-            int x = 5;
-            int y = 8;
-            int z = 0;
-            app.Use(async (context, next) =>
-            {
-                z = x * y;
-                await next.Invoke();
-            });
+            app.Map("/index", Index);
+            app.Map("/about", About);
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync($"x * y = {z}");
+                await context.Response.WriteAsync("Page Not Found");
+            });
+        }
+
+        private static void Index(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Index");
+            });
+        }
+        private static void About(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("About");
             });
         }
     }
