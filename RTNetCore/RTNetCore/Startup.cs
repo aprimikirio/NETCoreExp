75051acb2f6d2a9b4aea0f8f5bed7f7517f09f7e
APrimikirio <a.d.primikirio@gmail.com>
2018-10-19 23:05:41 +0300

    add use and run methods

--- RTNetCore/RTNetCore/Startup.cs
+++ RTNetCore/RTNetCore/Startup.cs
@@ -21,11 +21,18 @@ namespace RTNetCore
         }
         public void Configure(IApplicationBuilder app)
         {
-            int x = 0;
+            int x = 5;
+            int y = 8;
+            int z = 0;
+            app.Use(async (context, next) =>
+            {
+                z = x * y;
+                await next.Invoke();
+            });
+
             app.Run(async (context) =>
             {
-                x++;
-                await context.Response.WriteAsync($"request counter: {x}");
+                await context.Response.WriteAsync($"x * y = {z}");
             });
         }
     }
