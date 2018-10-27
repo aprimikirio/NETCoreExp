616dc3d68738bc633539bdce286eff2c56374d80
APrimikirio <a.d.primikirio@gmail.com>
2018-10-27 23:28:30 +0300

    add sessions

--- RTNetCore/RTNetCore/Startup.cs
+++ RTNetCore/RTNetCore/Startup.cs
@@ -3,29 +3,33 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
 
 namespace RTNetCore
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
-        { }
+        {
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+        }
 
         public void Configure(IApplicationBuilder app)
         {
             //app.UseDefaultFiles();
             //app.UseStaticFiles();
-
-            app.Use(async (context, next) =>
-            {
-                context.Items["text"] = "Vsem zdarova, pacani!!!";
-                await next.Invoke();
-            });
+            app.UseSession();
 
             app.Run(async (context) =>
             {
-                context.Response.ContentType = "text/html; charset=utf-8";
-                await context.Response.WriteAsync($"Текст: {context.Items["text"]}");
+                if (context.Session.Keys.Contains("name"))
+                    await context.Response.WriteAsync($"Hello {context.Session.GetString("name")}!");
+                else
+                {
+                    context.Session.SetString("name", "Tom");
+                    await context.Response.WriteAsync("Hello World!");
+                }
             });
         }
     }
