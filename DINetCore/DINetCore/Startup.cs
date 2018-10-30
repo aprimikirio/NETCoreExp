1abf441730195998b4a0bfb201945bce91cd8231
APrimikirio <a.d.primikirio@gmail.com>
2018-10-30 22:21:54 +0200

    add message sender service

--- DINetCore/DINetCore/Startup.cs
+++ DINetCore/DINetCore/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System.Text;
+using DINetCore.Services;
 
 namespace DINetCore
 {
@@ -11,28 +12,19 @@ namespace DINetCore
         private IServiceCollection _services;
         public void ConfigureServices(IServiceCollection services)
         {
-            _services = services;
+            services.AddTransient<IMessageSender, SmsMessageSender>();
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IMessageSender messageSender)
         {
-            app.Run(async context =>
+            if (env.IsDevelopment())
             {
-                var sb = new StringBuilder();
-                sb.Append("<h1>All services</h1>");
-                sb.Append("<table>");
-                sb.Append("<tr><th>Type</th><th>Lifetime</th><th>Realization</th></tr>");
-                foreach (var svc in _services)
-                {
-                    sb.Append("<tr>");
-                    sb.Append($"<td>{svc.ServiceType.FullName}</td>");
-                    sb.Append($"<td>{svc.Lifetime}</td>");
-                    sb.Append($"<td>{svc.ImplementationType?.FullName}</td>");
-                    sb.Append("</tr>");
-                }
-                sb.Append("</table>");
-                context.Response.ContentType = "text/html;charset=utf-8";
-                await context.Response.WriteAsync(sb.ToString());
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(messageSender.Send());
             });
         }
     }
