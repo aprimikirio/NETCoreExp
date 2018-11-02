02442935e9c90b13d84e9f768184606561d2d8e8
APrimikirio <a.d.primikirio@gmail.com>
2018-11-02 20:02:56 +0200

    add new MVC project

---
+++ MVCGame/MVCGame/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCGame.Models
+{
+    public class Room
+    {
+        public int Id;
+        public string Name;
+    }
+}
