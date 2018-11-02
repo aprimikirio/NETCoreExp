a4ce996f45609375238aeb320f1ece81e2c4547d
feisal13 <audatfeis@gmail.com>
2018-11-02 19:49:12 +0200

    add new MVC project

---
+++ MVCGame/MVCGame/Models/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCGame.Models
+{
+    public class Class
+    {
+        public int Id;
+        public int RoomId;
+        public int Edge;
+        public int X;
+        public int Y;
+    }
+}
