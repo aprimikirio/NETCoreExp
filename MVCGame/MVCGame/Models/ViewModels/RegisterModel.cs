6ce69b377716879bfa3c5c343d433bd0927af279
APrimikirio <a.d.primikirio@gmail.com>
2018-12-06 23:56:07 +0200

    add changes to css

---
+++ MVCGame/MVCGame/Models/ViewModels/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCGame.Models.ViewModels
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Email is empty")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is empty")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароль введен неверно")]
+        public string ConfirmPassword { get; set; }
+    }
+}
